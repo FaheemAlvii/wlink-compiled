c8497986bd3fc8068b3770bffc6721d53c12523c
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-11 17:02:09 +0500

    Update __manifest__.py

--- gts_contacts_whatsapp/__manifest__.py
+++ gts_contacts_whatsapp/__manifest__.py
@@ -3,8 +3,6 @@
     'description': 'static/description/index.html',
     'author': 'WLink',
     'license': 'LGPL-3',
-    'price':'0',
-    'currency':'usd',
     'version': '17.0.1.0',
     'depends': ['gts_whatsapp', 'contacts', 'sale'],
     'data': [
