136483a0c89976f43c7f30072845ca7761b3c1bb
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-11 16:14:12 +0500

    Update __manifest__.py

--- gts_whatsapp/__manifest__.py
+++ gts_whatsapp/__manifest__.py
@@ -4,6 +4,7 @@
     'license': 'LGPL-3',
     'version': '17.0.1.0',
     'depends': ['mail'],
+    'description':'static/description/index.html',
     'data': [
         'security/ir.model.access.csv',
         'views/connections.xml',
