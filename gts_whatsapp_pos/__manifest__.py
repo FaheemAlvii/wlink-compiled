db6471d7f47607cffc3d7ab3a2ce3919426b1b2e
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-11 16:14:54 +0500

    Update __manifest__.py

--- gts_whatsapp_pos/__manifest__.py
+++ gts_whatsapp_pos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Whatsapp Messaging for POS',
-    'description': "Adds whatsapp integration to the Point of Sale app.",
+    'description':'static/description/index.html',
     'author': 'WLink',
     'license': 'LGPL-3',
     'version': '17.0.1.0',
