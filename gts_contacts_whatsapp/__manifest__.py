6a00e29d81994ddb9d68c49323cc4b86f43f4840
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-20 21:07:23 +0500

    Contacts App Added

---
+++ gts_contacts_whatsapp/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Qr Whatsapp in Contacts app',
+    'description':'static/description/index.html',
+    'author': 'WLink',
+    'license': 'LGPL-3',
+    'version': '16.0.1.0',
+    'depends': ['gts_whatsapp', 'contacts', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'wizard/message_menu.xml',
+
+        'views/menu.xml'
+    ],
+    'installable': True,
+    'application': False
+}
