6882bf89a935b986cb1b227b5b0c0714ea2c4644
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-10-03 22:17:01 +0500

    Update __manifest__.py

--- gts_whatsapp_pos/__manifest__.py
+++ gts_whatsapp_pos/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'version': '17.0.1.0',
     'depends': ['gts_whatsapp', 'gts_contacts_whatsapp', 'point_of_sale', 'account'],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner_pos.png'],
     'website':'https://wlink.geektechsol.com',
     'data': [
         'security/ir.model.access.csv',
