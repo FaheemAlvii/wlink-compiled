b4c3ecbdaf584d28f31791d097de11944349ceca
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-10-03 22:16:32 +0500

    Update __manifest__.py

--- gts_whatsapp_integration_most/__manifest__.py
+++ gts_whatsapp_integration_most/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'version': '17.0.1.0',
     'website':'https://wlink.geektechsol.com',
-    'images': ['static/description/banner.png',],
+    'images': ['static/description/banner_docs.png',],
     'depends': ['gts_whatsapp', 'stock', 'purchase', 'account', 'sale', 'gts_contacts_whatsapp'],  # gts_contacts_whatsapp to re-use the messaging menu.
     'data': [
         'security/ir.model.access.csv',
